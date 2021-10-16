b66e3dbc9413870cd61c884ca9fcaaa04e7b698e
fernando <luizfcrj@hotmail.com>
2021-10-16 14:32:07 +0100

    Corrigindo de > para >>

--- lcd_init_xbits.c
+++ lcd_init_xbits.c
@@ -2,13 +2,13 @@
 /*                                                                          */
 /* Driver para Displays LCDs baseados no chip Hitachi HD44780               */
 /*                                                                          */
-/* Versão 1.0.3     (V20211012)                                             */
+/* Versão 1.0.5     (V20211015)                                             */
 /*                                                                          */
 /* Autores: Fernando da Cunha Luiz                                          */
 /*          Halysson Carvalho Junior                                        */
 /*                                                                          */
 /* Criado em : 12 de outubro de 2021                                        */
-/* Revisão em: 12 de outubro de 2021                                        */
+/* Revisão em: 15 de outubro de 2021                                        */
 /*                                                                          */
 /****************************************************************************/
 
@@ -122,7 +122,9 @@ void digitalWrite(unsigned char pino, unsigned char valor){
 
 static void setup(void){ // Setup do hardware:
 
+#if DEPURACAO
     printf_depuracao("\nsetup...\n");
+#endif // DEPURACAO
     lcd_init();
 
     return;
@@ -184,14 +186,21 @@ void lcd_print(char *frase, int8_t linha){
     // Obs.: Apesar de LCD_TAM_LINHA estar definido, a posição atual do cursor
     // não está sendo verificada => implementação futura.
 
+#if DEPURACAO
     printf_depuracao("\n   lcd_print:  \"");
+#endif // DEPURACAO
+
     for (int8_t i = 0; i < tamanho_linha; i++){
         // Envia dado para o display LCD:
         lcd_envia_comando_dado((uint8_t)frase[i], LCD_TIPO_DADO);
+#if DEPURACAO
         printf_depuracao("%c", (uint8_t)frase[i]);
+#endif // DEPURACAO
     } // for (int8_t i
-    printf_depuracao("\"");
 
+#if DEPURACAO
+    printf_depuracao("\"");
+#endif // DEPURACAO
 } // lcd_print(...
 
 /****************************************************************************/
@@ -200,49 +209,66 @@ void lcd_init(void){
 
     uint8_t lcd_comando = 0;
 
+#if DEPURACAO
     printf_depuracao("\nIniciando GPIOs para o Display LCD\n");
+#endif // DEPURACAO
 
-    delay(40); // Mínimo de 15ms para 5V ou 40ms para 2,7V, após alimentado.
+    delay(15); // Mínimo de 15ms para 5V ou 40ms para 2,7V, após alimentado
 
     // Iniciando EN e RS do display LCD:
+#if DEPURACAO
     printf_depuracao("\n");
     printf_depuracao("   LCD_PINO_EN = %u\n", LOW);
+#endif // DEPURACAO
     digitalWrite(LCD_PINO_EN, LOW); // Desabilitando pulso do EN
-    delay(1);
+    delayMicroseconds(1);
+
+#if DEPURACAO
     printf_depuracao("   LCD_PINO_RS = %u\n", LOW);
+#endif // DEPURACAO
+
     digitalWrite(LCD_PINO_RS, LOW); // Enviando comando
-    delay(1);
+    delayMicroseconds(1);
 
     // Display esta no modo interface 8-bits.
 
-    printf_depuracao("\nConfigurando o Display LCD\n");
+#if DEPURACAO
+    printf_depuracao("\nConfigurando o Display LCD, linha %d\n", __LINE__);
+#endif // DEPURACAO
 
     // Iniciando modo interface 8-bits:
     digitalWrite(LCD_BIT_7, LOW); // 0x30, bits 7 e 6 em baixo + bits 5 e 4 em alto
     digitalWrite(LCD_BIT_6, LOW);
     digitalWrite(LCD_BIT_5, HIGH);
     digitalWrite(LCD_BIT_4, HIGH);
-    printf_depuracao("\n");
-    printf_depuracao("      LCD_BIT_7 LCD_BIT_6 LCD_BIT_5 LCD_BIT_4 = %u %u %u %u \n", LOW, LOW, HIGH, HIGH);
+
+#if DEPURACAO
+    printf_depuracao("\n      LCD_BIT_7 LCD_BIT_6 LCD_BIT_5 LCD_BIT_4 = %u %u %u %u \n", LOW, LOW, HIGH, HIGH);
+#endif // DEPURACAO
 
     // Envia 0x30 3 vezes:
     lcd_en_pulse(); // Pulsa pino EN do display
     delay(5); // Mínimo de 4,1ms !!!
 
     lcd_en_pulse(); // Pulsa pino EN do display
-    delay(1); // Mínimo de 100uS !!!
+    delayMicroseconds(100); // Mínimo de 100uS !!!
 
     lcd_en_pulse(); // Pulsa pino EN do display
 
     // Display esta no modo interface 8-bits.
 
 #ifdef LCD_MODO_4_BITS
+#if DEPURACAO
     printf_depuracao("\n   Configurando no modo interface 4-bits... \n");
+#endif // DEPURACAO
     // Display esta no modo interface 8-bits.
     // Bits 7 e 6 em baixo + bit 5 em alto + bit 4 em alto
     // DL = Bit 4 = 0 => Troca para modo interface 4-bits
     digitalWrite(LCD_BIT_4, LOW); // 0x20, bit 4 em baixo
-    printf_depuracao("\n      LCD_BIT_7 LCD_BIT_6 LCD_BIT_5 LCD_BIT_4 = %u %u %u %u \n", LOW, LOW, HIGH, LOW);
+
+#if DEPURACAO
+    //printf_depuracao("\n      LCD_BIT_7 LCD_BIT_6 LCD_BIT_5 LCD_BIT_4 = %u %u %u %u \n", LOW, LOW, HIGH, LOW);
+#endif // DEPURACAO
     // Envia 0x20:
     lcd_en_pulse(); // Pulsa pino EN do display
 #endif // LCD_MODO_4_BITS
@@ -251,13 +277,17 @@ void lcd_init(void){
     // DL = Bit 4 = 0 => Modo interface 4-bits
     lcd_comando = ((HIGH << 5) | (LOW << 4)) ; // 0x20, bit 5 em alto e bit 4 em baixo
     // Display esta no modo interface 4-bits.
+#if DEPURACAO
     printf_depuracao("\n   Configurado no modo interface 4-bits\n");
+#endif // DEPURACAO
 #else // LCD_MODO_8_BITS:
     // Display esta no modo interface 8-bits.
     // DL = Bit 4 = 1 => Modo interface 8-bits
     lcd_comando = ((HIGH << 5) | (HIGH << 4)) ; // 0x30, bit 5 e bit 4 em alto
+#if DEPURACAO
     // Display esta no modo interface 8-bits.
     printf_depuracao("\n   Configurado no modo interface 8-bits\n");
+#endif // DEPURACAO
 #endif // LCD_MODO_...
     // Configura para 2 linhas e fonte 5 X 8:
     lcd_comando |= (1 << 3); // N, Linhas, N = 1 => 2 linhas, N = 0 => 1 linha
@@ -280,12 +310,10 @@ void lcd_init(void){
 /****************************************************************************/
 
 void lcd_en_pulse(void){ // Pulsa pino EN do display
-
-    delay(1); // Mínimo de 1us
+    delayMicroseconds(1); // Mínimo de 1us
     digitalWrite(LCD_PINO_EN, HIGH);
-    delay(1); // Mínimo de 1us
+    delayMicroseconds(1); // Mínimo de 1us
     digitalWrite(LCD_PINO_EN, LOW);
-
 } // lcd_en_pulse()
 
 /****************************************************************************/
@@ -294,7 +322,9 @@ void lcd_en_pulse(void){ // Pulsa pino EN do display
 void lcd_envia_comando_dado(uint8_t comando, uint8_t tipo_comando_ou_dado){
 
     if (tipo_comando_ou_dado == LCD_TIPO_COMANDO){
+#if DEPURACAO
         printf_depuracao("\n   Comando... ");
+#endif // DEPURACAO
         digitalWrite(LCD_PINO_RS, LOW); // Enviando comando
     } //
     else{ // (tipo_comando_ou_dado == LCD_TIPO_DADO)
@@ -305,34 +335,50 @@ void lcd_envia_comando_dado(uint8_t comando, uint8_t tipo_comando_ou_dado){
     digitalWrite(LCD_BIT_6, !(!(comando & (1 < 6))));
     digitalWrite(LCD_BIT_5, !(!(comando & (1 < 5))));
     digitalWrite(LCD_BIT_4, !(!(comando & (1 < 4))));
+
 #ifdef LCD_MODO_8_BITS
+#if DEPURACAO
     if (tipo_comando_ou_dado == LCD_TIPO_COMANDO){
         printf_depuracao("      LCD 8 bits = ");
         printf_depuracao("0x%1X", (comando & 0xf0) >> 4);
     }
+#endif // DEPURACAO
+
     digitalWrite(LCD_BIT_3, !(!(comando & (1 < 3))));
     digitalWrite(LCD_BIT_2, !(!(comando & (1 < 2))));
     digitalWrite(LCD_BIT_1, !(!(comando & (1 < 1))));
     digitalWrite(LCD_BIT_0, !(!(comando & (1 < 0))));
+
+#if DEPURACAO
     if (tipo_comando_ou_dado == LCD_TIPO_COMANDO){
         printf_depuracao("%1X\n", (comando & 0x0f));
     }
+#endif // DEPURACAO
+
     // Envia 8 bits para o LCD:
     lcd_en_pulse(); // Pulsa pino EN do display
 #else // LCD_MODO_4_BITS
+#if DEPURACAO
     if (tipo_comando_ou_dado == LCD_TIPO_COMANDO){
         printf_depuracao("      LCD 4 bits = ");
         printf_depuracao("0x%1X", (comando & 0xf0));
     }
+#endif // DEPURACAO
+
     // Envia o nibble mais significativo para o LCD:
     lcd_en_pulse(); // Pulsa pino EN do display
+
     digitalWrite(LCD_BIT_7, !(!(comando & (1 < 3))));
     digitalWrite(LCD_BIT_6, !(!(comando & (1 < 2))));
     digitalWrite(LCD_BIT_5, !(!(comando & (1 < 1))));
     digitalWrite(LCD_BIT_4, !(!(comando & (1 < 0))));
+
+#if DEPURACAO
     if (tipo_comando_ou_dado == LCD_TIPO_COMANDO){
         printf_depuracao(" 0x%1X\n", (comando & 0x0f) << 4);
     }
+#endif // DEPURACAO
+
     // Envia o nibble menos significativo para o LCD:
     lcd_en_pulse(); // Pulsa pino EN do display
 #endif // LCD_MODO_...
@@ -340,6 +386,5 @@ void lcd_envia_comando_dado(uint8_t comando, uint8_t tipo_comando_ou_dado){
 } // lcd_envia_comando_dado(...
 
 /****************************************************************************/
-/*  FIM                       */
+/*  FIM                                                                     */
 /****************************************************************************/
-
